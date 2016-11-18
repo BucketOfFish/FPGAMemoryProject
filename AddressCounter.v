fe32aea0c8644baff2c32cf52e07d66d30506dbe
Matt Zhang <set.stun@gmail.com>
2016-11-17 21:21:44 -0600

    Renamed and cleaned up some stuff. Did not test.

--- AddressCounter.v
+++ AddressCounter.v
@@ -8,7 +8,7 @@ module AddressCounter(
 `include "MyParameters.vh"
 
 input clock, storageReady;
-output reg [LETTERINDEXBITS+WORDINDEXBITS-1:0] address = WORDLENGTH*MEMORYDEPTH-1;
+output reg [COLINDEXBITS+ROWINDEXBITS-1:0] address = WORDLENGTH*MEMNROWS-1;
 output reg newAddress;
 
 reg alreadyLooped = 0;
@@ -19,7 +19,7 @@ reg alreadyLooped = 0;
     //if (storageReady && !alreadyLooped) begin
         //newAddress = 0; // this particular module is always sending stuff to memory, so right now this is not really necessary.
         //address = address + 1;
-        //if (address == WORDLENGTH*MEMORYDEPTH-1) begin
+        //if (address == WORDLENGTH*MEMNROWS-1) begin
             //alreadyLooped = 1;
         //end
         //newAddress = 1;
@@ -27,10 +27,9 @@ reg alreadyLooped = 0;
 //end
 
 // loops through all addresses in array
-reg [3:0] xPos[22:0] = {8, 8, 8, 5, 11, 14, 11, 1, 7, 5, 15, 8, 12, 4, 4, 7, 2, 1, 13, 0};
-reg [3:0] yPos[22:0] = {4, 3, 8, 8, 8,  0, 11, 12, 12, 3, 3, 1, 1, 12, 7, 7, 11, 15, 0, 4};
-//reg [3:0] xPos[7:0] = {4, 8, 8, 5, 11, 14, 11, 7};
-//reg [3:0] yPos[7:0] = {4, 3, 8, 8, 8,  0, 11, 12};
+// submit three sets of (0,0) first. this is a bug I need to fix, because the code ignores the first three lines.
+reg [3:0] xPos[22:0] = {0, 0, 0, 8, 8, 8, 5, 11, 14, 11, 1, 7, 5, 15, 8, 12, 4, 4, 7, 2, 1, 13, 0};
+reg [3:0] yPos[22:0] = {0, 0, 0, 4, 3, 8, 8, 8,  0, 11, 12, 12, 3, 3, 1, 1, 12, 7, 7, 11, 15, 0, 4};
 integer count = 0;
 
 always @(posedge clock) begin
--- Main.v
+++ Main.v
@@ -4,36 +4,26 @@ module Main();
 
 `include "MyParameters.vh"
 
-reg clock, clearStorage;
-wire [LETTERINDEXBITS+WORDINDEXBITS-1:0] address;
-wire [WORDINDEXBITS-1:0] wordIndex;
-wire [LETTERINDEXBITS-1:0] letterIndex;
+reg clock, clearMemory, readMemory;
+wire [COLINDEXBITS+ROWINDEXBITS-1:0] address;
+wire [ROWINDEXBITS-1:0] wordIndex;
+wire [COLINDEXBITS-1:0] letterIndex;
 wire storageReady, storedValue, newAddress, readReady;
-reg inquiry;
-integer inquiryCount;
-reg [WORDINDEXBITS-1:0] inquiryWordIndex;
-reg [LETTERINDEXBITS-1:0] inquiryLetterIndex;
+//reg inquiry;
+//reg [ROWINDEXBITS-1:0] inquiryWordIndex;
+//reg [COLINDEXBITS-1:0] inquiryLetterIndex;
 reg [31:0] randomArray;
 
 initial begin
     clock <= 1;
-    clearStorage <= 1;
-    inquiry <= 0;
-    inquiryCount <= 0;
+    clearMemory <= 1;
+    readMemory <= 0;
+    //inquiry <= 0;
 end
 
 always begin
     #1 clock = ~clock;
-    //#1; // it's necessary to put this stuff in the middle of a clock cycle to prevent race conditions.
-    if (clock) clearStorage = 0; // let clearStorage be high for one full cycle.
-    //inquiryCount = inquiryCount+1;
-    //if (inquiryCount == 2) begin
-        //inquiryCount = 0;
-        //inquiry = ~inquiry;
-        //randomArray = $random;
-        //inquiryWordIndex[WORDINDEXBITS-1:0] = randomArray[WORDINDEXBITS-1:0];
-        //inquiryLetterIndex[LETTERINDEXBITS-1:0] = randomArray[LETTERINDEXBITS+WORDINDEXBITS-1:WORDINDEXBITS];
-    //end
+    if (clock) clearMemory = 0; // let clearMemory be high for one full cycle.
 end
 
 AddressCounter newCounter(
@@ -54,13 +44,12 @@ BlockMemoryStorage newStorage(
     .clock(clock),
     .wordIndex(wordIndex),
     .letterIndex(letterIndex),
-    //.wordIndex(letterIndex),
-    //.letterIndex(wordIndex),
     .newAddress(newAddress),
-    .inquiry(inquiry),
-    .inquiryWordIndex(inquiryWordIndex),
-    .inquiryLetterIndex(inquiryLetterIndex),
-    .clearStorage(clearStorage),
+    //.inquiry(inquiry),
+    //.inquiryWordIndex(inquiryWordIndex),
+    //.inquiryLetterIndex(inquiryLetterIndex),
+    .clearMemory(clearMemory),
+    .readMemory(readMemory),
     .storageReady(storageReady),
     .readReady(readReady),
     .storedValue(storedValue)
