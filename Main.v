5abd473e1d43c1dd660478a7cd80b4ac43574cf3
Matt Zhang <set.stun@gmail.com>
2016-11-17 22:09:24 -0600

    Added variables for new memory structures - incomplete.

--- Main.v
+++ Main.v
@@ -7,16 +7,12 @@ module Main();
 reg clock, clearMemory, readMemory;
 wire [COLINDEXBITS+ROWINDEXBITS-1:0] address;
 wire storageReady, storedValue, newAddress, readReady;
-//reg inquiry;
-//reg [ROWINDEXBITS-1:0] inquiryWordIndex;
-//reg [COLINDEXBITS-1:0] inquiryLetterIndex;
 reg [31:0] randomArray;
 
 initial begin
     clock <= 1;
     clearMemory <= 1;
     readMemory <= 0;
-    //inquiry <= 0;
 end
 
 always begin
@@ -35,9 +31,6 @@ BlockMemoryStorage newStorage(
     .clock(clock),
     .address(address),
     .newAddress(newAddress),
-    //.inquiry(inquiry),
-    //.inquiryWordIndex(inquiryWordIndex),
-    //.inquiryLetterIndex(inquiryLetterIndex),
     .clearMemory(clearMemory),
     .readMemory(readMemory),
     .storageReady(storageReady),
